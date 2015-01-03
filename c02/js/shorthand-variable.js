aff380843d6010c82a23ecd4d9ca42498c0eedfe
leopoldkwok <leopoldkwok@gmail.com>
2015-01-03 23:26:04 +0000

    shorthand for creating variables

---
+++ c02/js/shorthand-variable.js
@@ -0,0 +1,16 @@
+var price = 5;
+var quantity = 14;
+var total = price * quantity;
+
+var price, quantity, total;
+price = 5;
+quantity = 14;
+total = price * qantity;
+
+var price = 5, quantity = 14;
+var total = price * quantity;
+
+//write total into the element with id of cost
+var el = document.getElementById('cost');
+el.textContent = '$' + total;
+
